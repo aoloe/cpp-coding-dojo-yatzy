9a0e1a288ac4061bf3383d21aefeaedaf8d6b8df
ale rimoldi <ale@graphicslab.org>
2019-03-18 20:22:39 +0100

    4.1.b (twos) refactoring switch

--- src/yatzy.cpp
+++ src/yatzy.cpp
@@ -7,14 +7,23 @@ namespace Yatzy {
 
 int score(Categories categories, std::array<int, 5> dice)
 {
-    if (categories == Categories::chance) {
-        return std::accumulate(dice.begin(), dice.end(), 0);
-    } else if (categories == Categories::yatzy) {
-        return std::equal(dice.begin() + 1, dice.end(), dice.begin()) ? 50 : 0;
-    } else if (categories == Categories::fours) {
-        return std::count(dice.begin(), dice.end(), 4) * 4;
-    } else if (categories == Categories::twos) {
-        return std::count(dice.begin(), dice.end(), 2) * 2;
+    switch (categories) {
+        case Categories::chance:
+            return std::accumulate(dice.begin(), dice.end(), 0);
+        case Categories::yatzy:
+            return std::equal(dice.begin() + 1, dice.end(), dice.begin()) ? 50 : 0;
+        case Categories::ones:
+        break;
+        case Categories::twos:
+            return std::count(dice.begin(), dice.end(), 2) * 2;
+        case Categories::threes:
+        break;
+        case Categories::fours:
+            return std::count(dice.begin(), dice.end(), 4) * 4;
+        case Categories::fives:
+        break;
+        case Categories::sixes:
+        break;
     }
     return 0;
 }
