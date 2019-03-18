0f140857fd289ac8eafdeee369a104fdfcbc8cb7
ale rimoldi <ale@graphicslab.org>
2019-03-18 19:54:02 +0100

    2 ok

--- src/yatzy.cpp
+++ src/yatzy.cpp
@@ -1,9 +1,12 @@
 #include "yatzy.hpp"
+
+#include <numeric>
+
 namespace Yatzy {
 
-int score(Categories, std::array<int, 5>)
+int score(Categories, std::array<int, 5> dice)
 {
-	return 14;
+    return std::accumulate(dice.begin(), dice.end(), 0);
 }
 
 }
