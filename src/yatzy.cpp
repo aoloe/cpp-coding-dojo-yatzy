0caa22394e84d76dabeced4a89c60c32940c8548
ale rimoldi <ale@graphicslab.org>
2019-03-18 20:10:10 +0100

    4.1 (ones, twos, ...) ok

--- src/yatzy.cpp
+++ src/yatzy.cpp
@@ -9,9 +9,12 @@ int score(Categories categories, std::array<int, 5> dice)
 {
     if (categories == Categories::chance) {
         return std::accumulate(dice.begin(), dice.end(), 0);
-    } else {
+    } else if (categories == Categories::yatzy) {
         return std::equal(dice.begin() + 1, dice.end(), dice.begin()) ? 50 : 0;
+    } else if (categories == Categories::fours) {
+        return 8;
     }
+    return 0;
 }
 
 }
