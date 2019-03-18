9a0531d0880d923ec6470b6a8e46601144ecf2e1
ale rimoldi <ale@graphicslab.org>
2019-03-18 19:57:06 +0100

    3.2 (yatzy) ok

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,6 @@ int main()
 	assert(Yatzy::score(Yatzy::Categories::chance, {1,1,3,3,6}) == 14);
 	assert(Yatzy::score(Yatzy::Categories::chance, {4,5,5,6,1}) == 21);
 	assert(Yatzy::score(Yatzy::Categories::yatzy, {1,1,1,1,1}) == 50);
+	assert(Yatzy::score(Yatzy::Categories::yatzy, {5,5,5,5,5}) == 50);
     
 }
