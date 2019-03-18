6de5a96d27889b4d655842c8d1b43a42d07f4e9f
ale rimoldi <ale@graphicslab.org>
2019-03-18 20:03:24 +0100

    3.3 (yatzy) ok

--- src/yatzy.cpp
+++ src/yatzy.cpp
@@ -1,6 +1,7 @@
 #include "yatzy.hpp"
 
 #include <numeric>
+#include <algorithm>
 
 namespace Yatzy {
 
@@ -9,7 +10,7 @@ int score(Categories categories, std::array<int, 5> dice)
     if (categories == Categories::chance) {
         return std::accumulate(dice.begin(), dice.end(), 0);
     } else {
-        return 50;
+        return std::equal(dice.begin() + 1, dice.end(), dice.begin()) ? 50 : 0;
     }
 }
 
