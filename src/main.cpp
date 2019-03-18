f9aebbe2d52171479ff2a40f745f27435ff4d288
ale rimoldi <ale@graphicslab.org>
2019-03-18 20:34:39 +0100

    4.1.d (threes) fail

--- src/main.cpp
+++ src/main.cpp
@@ -14,4 +14,5 @@ int main()
 	assert(Yatzy::score(Yatzy::Categories::fours, {1,1,2,4,3}) == 4);
 	assert(Yatzy::score(Yatzy::Categories::twos, {2,3,2,5,1}) == 4);
 	assert(Yatzy::score(Yatzy::Categories::ones, {2,3,2,5,1}) == 1);
+	assert(Yatzy::score(Yatzy::Categories::threes, {3,3,3,5,1}) == 9);
 }
